b01f254d2a236c00156ea5ce6910fa5a99360052
Aleksandr Zhitnikov <zhivoysp@gmail.com>
2025-06-25 21:30:09 +0300

    chore: tidy React components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 import './Header.css';
 
 const Header = ({ minimal = false }) => (
@@ -7,5 +7,9 @@ const Header = ({ minimal = false }) => (
     {!minimal && <img src="/fish.png" alt="Fish" className="fish-image" />}
   </header>
 );
+
+Header.propTypes = {
+  minimal: PropTypes.bool,
+};
 
 export default Header;
